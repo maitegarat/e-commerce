e52befc4da61d45dba28df629bbd8cc2171bc35b
Maite Garat <maitegarat@hotmail.com>
2021-08-21 21:07:09 -0300

    entrega 01

--- js/login.js
+++ js/login.js
@@ -1,6 +1,44 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+function mostrarpwd() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+};
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+  document.getElementById("logbttn").addEventListener("click", function() {
+
+    let inputEmail = document.getElementById("usuario");
+    let inputPassword = document.getElementById("password");
+    let camposCompletos = true;
+
+    if (inputEmail.value === '') {
+      inputEmail.classList.add("invalid");
+      camposCompletos = false;
+    } else {
+      inputEmail.classList.remove("invalid");
+    }
+
+    if (inputPassword.value === '') {
+      inputPassword.classList.add("invalid");
+      camposCompletos = false;
+    } else {
+      inputPassword.classList.remove("invalid");
+    }
+
+    if (camposCompletos) {
+      window.location = "inicio.html";
+    } else {
+      alert("Ingresar los datos por favor");
+    }
+
+  })
 
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,40 @@
+
+function cargarDatos(url) {
+    document.getElementById("list").innerHTML = "";
+   
+    fetch(url)
+        .then(respuesta => respuesta.json())
+       
+        .then(datos => {
+                      
+            for (let i = 0; i < datos.length; i++) {
+
+            let lista = "";
+            lista = `
+                        <div  id = "center`+ i +`">
+                            <p> <img src = "${ datos[i].imgSrc }" class = "imagsiz"> </p>
+                            <h3>${datos[i].name}</h3>
+                            <p class = "descrp">${datos[i].description}</p>
+                            <p class = "precio">${datos[i].cost} ${datos[i].currency}</p>
+                            <em> Artículos vendidos: ${datos[i].soldCount}</em>
+                        </div>
+                        
+                        `;
+
+                        document.getElementById("list").innerHTML += lista;
+            };
+
+           
+        
+        })
+        .catch(error => alert("Hubo un error: " + error));
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    cargarDatos(PRODUCTS_URL)
 });
